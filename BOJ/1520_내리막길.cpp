e391438766ec7f583c0051e6c9a6e2cb700120e1
cannalee90 <cannalee90@gmail.com>
2015-03-21 19:42:34 +0900

    DP

--- BOJ/1520_내리막길.cpp
+++ BOJ/1520_내리막길.cpp
@@ -15,30 +15,66 @@ typedef long long ll;
 
 using namespace std;
 
+int mmove[4][2] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+vector<vector<long> > ground;
+vector < vector<bool> >  visited;
+int ans;
+int height;
+int width;
+
+bool isPossible(int h, int w, long long latitude){
+	if (h >= height || w >= width || h < 0 || w < 0)
+		return false;
+	if (latitude >= ground[h][w])
+		return false;
+	return true;
+}
+
 
 int main()
 {
 #ifdef _CONSOLE
 	freopen("input.txt", "r", stdin);
 #endif
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		string a, b;
-		cin >> a >> b;
-		cout << "Distances: ";
-		for (int i = 0; i < a.size(); i++)
-		{
-			if (b[i] >= a[i])
-			{
-				printf("%d ", b[i] - a[i]);
-			}
-			else
-			{
-				printf("%d ", b[i] + 26 - a[i]);
+	cin >> height >> width;
+	ground.resize(height + 1, vector<long>(width + 1));
+	visited.resize(height + 1, vector<bool>(width + 1));
+	for (int i = 0; i < height; i++){
+		for (int j = 0; j < width; j++){
+			cin >> ground[i][j];
+		}
+	}
+	vector<vector<int> > visited(height + 1, vector<int>(width + 1));
+	visited[0][0] = 1;
+	for (int i = 0; i < height; i++){
+		for (int j = 0; j < width; j++){
+
+			for (int k = 0; k < 4; k++){
+				if (isPossible(i + mmove[k][0], j + mmove[k][1], ground[i][j])){
+					visited[i][j] += visited[i + mmove[k][0]][j + mmove[k][1]];
+				}
 			}
 		}
-		printf("\n");
 	}
+
+	while (1){
+		vector<vector<int > > DP = visited;
+		visited.clear();
+		visited.resize(height + 1, vector<int>(width + 1, 0));
+		visited[0][0] = 1;
+
+		for (int i = 0; i < height; i++){
+			for (int j = 0; j < width; j++){
+
+				for (int k = 0; k < 4; k++){
+					if (isPossible(i + mmove[k][0], j + mmove[k][1], ground[i][j])){
+						visited[i][j] += DP[i + mmove[k][0]][j + mmove[k][1]];
+					}
+				}
+			}
+		}
+		if (DP == visited)
+			break;
+	}
+	cout << visited[height - 1][width - 1] << endl;
 }
