1bd74dc9ddafc35705b47f1fa40efd236264457f
cannalee90 <cannalee90@gmail.com>
2015-03-24 09:47:19 +0900

    dijkstra tracking, DAG, Topological Sort

--- BOJ/1948_임계경로.cpp
+++ BOJ/1948_임계경로.cpp
@@ -1,96 +1,85 @@
 #include <iostream>
+#include <limits.h>
+#include <utility>
 #include <fstream>
 #include <string>
 #include <string.h>
 #include <queue>
 #include <stdio.h>
 #include <algorithm>
+#include <set>
+#include <map>
+#include <functional>
 #include <vector>
 #include <stack>
-#include <set>
 
-#define INF 900000000
-  
-  
 typedef long long ll;
 using namespace std;
-  
-int n, m;
-int bbegin, eend;
-int total;
-int check[10010];
-bool map[10010][10010];
-vector< vector<pair<int, int> > > graph;
-int result[10010];
-int final;
-int route[10010];
 
+long long ans;
+vector<vector<bool> > edges;
+vector<bool>  visited;
 
- 
-int main()
-{
+int main(){
 #ifdef _CONSOLE
-    freopen("input.txt", "r", stdin);
+	freopen("input.txt", "r", stdin);
 #endif
-    int t1,t2,t3;
-	vector<pair<int, int> > total;
-    cin >> n >> m;
-	graph.resize(n+1);
-    for(int i = 0; i<m; i++)
-    {
-		scanf("%d %d %d", &t1, &t2, &t3);
-        graph[t1].push_back(make_pair(t2,t3));
-        map[t1][t2] = 1;
-    }
-    cin >> bbegin >> eend;
-    priority_queue< pair<int, int> , vector <pair<int, int> >, less<pair<int, int> > > pq;
-    vector<ll> distance(n + 1,0);
-    pq.push(make_pair(0,bbegin));
-	route[bbegin] = -1;
-    while(!pq.empty())
-    {
-        int here = pq.top().second;
-        int here_dis = pq.top().first;
-        if(distance[here] > here_dis)
-        {
-            pq.pop();
-            continue;
-        }
-        pq.pop();
-        for(int i = 0; i<graph[here].size(); i++)
-        {
-            int there = graph[here][i].first;
-            int there_dis = graph[here][i].second;
-            if(distance[there] <= there_dis + here_dis)
-            {
-				route[there] = here;
-				if(there == eend)
-				{
-					if(distance[there] < there_dis + here_dis)
-					{
-						total.clear();
-					}
-					int h = route[there], t = eend;
-					while(h != -1)
-					{
-						total.push_back(make_pair(h, t));
-						t = h;
-						h = route[h];
-					}
-				}
-                distance[there] = there_dis + here_dis;
-                pq.push(make_pair(there_dis + here_dis, there));
-            }
-        }
-    }
-	int cnt  = 0;
-	for(int i = 0; i<total.size(); i++)
-	{
-		int here = total[i].first;
-		int there = total[i].second;
-		cnt += map[here][there];
-		map[here][there] = 0;
+	int n, m;
+	cin >> n >> m;
+	vector<vector<pair<int, int> > > graph(n + 1);
+	vector<vector<pair<int, int> > > track(n + 1);
+	edges.resize(n + 1, vector<bool>(n + 1, false));
+	for (int i = 0; i < m; i++){
+		int h, t, w;
+		scanf("%d %d %d", &h, &t, &w);
+		graph[h].push_back(make_pair(t, w));
+		track[t].push_back(make_pair(h, w));
 	}
-    cout << distance[eend] << endl;
-	cout << cnt << endl;
+	int begin, end;
+	cin >> begin >> end;
+	priority_queue<pair<int, int>, vector<pair<int, int> >, less<pair<int, int> > > pq;
+	pq.push(make_pair(0, begin));
+	vector<int> dis(n + 1, INT_MIN);
+	vector<vector<int > > parent(n + 1);
+
+	dis[begin] = 0;
+
+	while (!pq.empty()){
+		int here = pq.top().second;
+		int here_dis = pq.top().first;
+		pq.pop();
+		for (int i = 0; i < graph[here].size(); i++){
+			int there = graph[here][i].first;
+			int cost = here_dis + graph[here][i].second;
+			if (dis[there] < cost){
+				dis[there] = cost;
+				parent[there].clear();
+				parent[there].push_back(here);
+				pq.push(make_pair(cost, there));
+			}
+			else if (dis[there] == cost)
+				parent[there].push_back(here);
+		}
+	}
+	visited.resize(n + 1, false);
+	long long ans = 0;
+	cout << dis[end] << endl;
+	visited[end] = true;
+	queue <int> v;
+	v.push(end);
+	while (!v.empty()){
+		int here = v.front();
+		v.pop();
+		if (here == begin)
+			continue;
+		for (int i = 0; i < parent[here].size(); i++){
+			int there = parent[here][i];
+			ans++;
+			if (!visited[there]){
+				visited[there] = true;
+				v.push(there);
+			}
+		}
+	}
+	cout << ans << endl;
 }
