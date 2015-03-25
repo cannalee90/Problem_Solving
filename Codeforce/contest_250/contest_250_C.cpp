6ba9aac7dad4e7f9aedc6b2ad33f7876ca10281e
cannalee90 <cannalee90@gmail.com>
2015-03-25 10:37:30 +0900

    graph, implement, idea

--- Codeforce/contest_250/contest_250_C.cpp
+++ Codeforce/contest_250/contest_250_C.cpp
@@ -1,31 +1,39 @@
+#include<iostream>
 #include <iostream>
+#include <utility>
 #include <fstream>
 #include <string>
 #include <string.h>
 #include <queue>
 #include <stdio.h>
 #include <algorithm>
+#include <limits.h>
+#include <set>
+#include <map>
 #include <vector>
 #include <stack>
-#include <map>
-#include <set>
-#include <limits.h>
 
 typedef long long ll;
+
 using namespace std;
 
-class edge{
-public:
-	int here;
-	int there;
-	int weight;
-	edge(int here, int there, int weight){
-		this->here = here;
-		this->there = there;
-		this->weight = weight;
+
+int main(){
+#ifdef _CONSOLE
+	freopen("input.txt", "r", stdin);
+#endif
+	int n, m;
+	cin >> n >> m;
+	vector<int> data(n + 1);
+	vector<vector<int > > graph(n + 1);
+	for (int i = 1; i <= n; i++){
+		cin >> data[i];
 	}
-	bool operator <(const edge &a) const
-	{
-		return this->weight > a.weight;
+	int ans = 0;
+	for (int j = 0; j<m; j++){
+		int t1, t2;
+		cin >> t1 >> t2;
+		ans += min(data[t1], data[t2]);
 	}
-};
+	cout << ans << endl;
+}
