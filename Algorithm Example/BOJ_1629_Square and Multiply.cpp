789e92f1b1b29543b0343fd08f0f1e6927a6caf1
cannalee90 <cannalee90@gmail.com>
2015-03-29 18:29:18 +0900

    squre and multiply

---
+++ Algorithm Example/BOJ_1629_Square and Multiply.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <limits.h>
+#include <utility>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <stack>
+
+typedef long long ll;
+
+using namespace std;
+
+ll squre_and_multiply(ll base, ll exp, ll mod){
+	ll ans = 1;
+	ll b = base;
+	while (exp){
+		if (exp & 1) 
+			ans *= b;
+		exp = exp >> 1; 
+		b *= b;
+		b %= mod;
+		ans %= mod;
+	}
+	return ans;
+}
+
+int main(){
+#ifdef _CONSOLE
+	freopen("input.txt", "r", stdin);
+#endif
+	long long a, b, c;
+	cin >> a >> b >> c; //a는 base b는 exponent c모듈러 연산용
+	cout << squre_and_multiply(a, b, c) << endl;
+}
