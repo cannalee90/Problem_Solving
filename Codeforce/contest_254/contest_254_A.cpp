2e7143b01eefd1ad076c0dab90889198175b34c2
cannalee90 <cannalee90@gmail.com>
2015-03-31 21:51:41 +0900

    implement, idea, char

---
+++ Codeforce/contest_254/contest_254_A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <limits.h>
+#include <utility>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <functional>
+#include <queue>
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <stack>
+
+typedef long long ll;
+
+using namespace std;
+
+int main(){
+#ifdef _CONSOLE
+	freopen("input.txt", "r", stdin);
+#endif
+	int n, m;
+	char r;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> r;
+			if (r == '-')
+				cout << r;
+			else if ((i + j) % 2 == 0)
+				cout << "B";
+			else
+				cout << "W";
+		}
+		cout << endl;
+	}
+	return 0;
+}
