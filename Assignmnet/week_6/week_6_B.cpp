4dd137c178c0d9dd516f79f2f88325cd04aa0190
cannalee90 <cannalee90@gmail.com>
2015-04-12 19:46:55 +0900

    graph modeling

--- Assignmnet/week_6/week_6_B.cpp
+++ Assignmnet/week_6/week_6_B.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
-#include <limits.h>
-#include <utility>
-#include <fstream>
 #include <string>
-#include <string.h>
-#include <functional>
-#include <queue>
-#include <stdio.h>
-#include <algorithm>
-#include <set>
-#include <map>
-#include <vector>
-#include <stack>
 
 typedef long long ll;
 
@@ -20,29 +8,18 @@ using namespace std;
 int graph[8][2] = { { 7, 1 }, { 2, 8 }, { 3, 8 }, { 3, 4 }, { 7, 5 }, { 6, 5 }, { 3, 0 }, { 8, 0 } };
 
 int main(){
-#ifdef _CONSOLE
-	freopen("input.txt", "r", stdin);
-#endif
 	int t;
-	std::ios::sync_with_stdio(false);
 	cin >> t;
 	while (t--){
 		int here = 0;
 		int idx = 0;
-		bool isFail = false;
 		string str;
 		cin >> str;
-		while(idx != str.size()){
+		while(idx != str.size() && here != 8){
 			int there = str[idx++] - '0';
 			here = graph[here][there];
-			if (here == 8){
-				isFail = true;
-				break;
-			}
 		}
-		if (isFail)
-			cout << "NO" << endl;
-		else if (here == 0 || here == 4 || here == 5 || here == 6)
+		if (here == 0 || here == 4 || here == 5 || here == 6)
 			cout << "YES" << endl;
 		else
 			cout << "NO" << endl;
