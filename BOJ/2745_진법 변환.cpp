d0d5296fda88306940d1e60f96c6e9df54f34b73
cannalee90 <cannalee90>
2015-05-22 22:13:15 +0900

    implement

---
+++ BOJ/2745_진법 변환.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+#include <limits.h>
+
+typedef long long ll;
+using namespace std;
+
+int n, m, k;
+
+
+int main()
+{
+#ifdef _CONSOLE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w+", stdout);
+#endif
+
+}
