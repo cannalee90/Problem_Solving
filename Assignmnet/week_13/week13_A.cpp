95b8fa009cc4df840d73e4e98842f96269050196
cannalee90 <cannalee90>
2015-06-01 17:45:47 +0900

    implment

--- Assignmnet/week_13/week13_A.cpp
+++ Assignmnet/week_13/week13_A.cpp
@@ -14,7 +14,12 @@
 typedef long long ll;
 using namespace std;
 
-vector<pair<int, int > > dots, group_a, group_b;
+
+inline bool cmp(const pair<ll, ll> & a, const pair<ll, ll> &b){
+	if (a.first == b.first)
+		return b.second < a.second;
+	return a.first < b.first;
+}
 
 int main()
 {
@@ -23,26 +28,41 @@ int main()
 #endif
 	int t;
 	cin >> t;
+//	ios::sync_with_stdio(false);
 	while (t--){
 		int n;
+		double slope;
 		cin >> n;
-		dots = vector<pair<int, int > >(n);
+		vector<pair<ll, ll > > dots, group_a, group_b;
+		dots = vector<pair<ll, ll> >(n);
 		for (int i = 0; i < n; i++) cin >> dots[i].first >> dots[i].second;
-		sort(dots.begin(), dots.end());
-		double slope = (double)(dots[n - 1].first - dots[0].first) / (double)(dots[n - 1].second - dots[0].second);
+		sort(dots.begin(), dots.end(), cmp);
+		if (dots[n - 1].first - dots[0].first == 0){
+			cout << "0" << endl;
+			return 0;
+		}
+		else 
+			slope = (double)((double)dots[n - 1].second - (double)dots[0].second) / (double)((double)dots[n - 1].first - (double)dots[0].first);
+
 		group_a.push_back(dots[0]);
-		group_a.push_back(dots[n - 1]);
 		group_b.push_back(dots[0]);
-		group_b.push_back(dots[n - 1]);
+
 		for (int i = 1; i < n - 1; i++){
-			double t_slope = (double)(dots[i].first - dots[0].first) / (double)(dots[i].second - dots[0].second);
+			double t_slope = (double)((double)dots[i].second - (double)dots[0].second) / (double)((double)dots[i].first - (double)dots[0].first);
 			if (t_slope >= slope)
 				group_a.push_back(dots[i]);
 			else
 				group_b.push_back(dots[i]);
 		}
-		for (int i = dots[0].first; i <= dots[n - 1].first; i++){
 
-		}
-	}
+		group_a.push_back(dots[n - 1]);
+		group_b.push_back(dots[n - 1]);
+
+		ll area = 0;
+		for (int i = 0; i < group_a.size() - 1; i++)
+			area += (ll)((group_a[i + 1].second + group_a[i].second) * (group_a[i + 1].first - group_a[i].first));
+		for (int i = 0; i < group_b.size() - 1; i++)
+			area -= (ll)((group_b[i + 1].second + group_b[i].second) * (group_b[i + 1].first - group_b[i].first));
+		cout << area * (ll)5000<< endl;
+	}	
 }
