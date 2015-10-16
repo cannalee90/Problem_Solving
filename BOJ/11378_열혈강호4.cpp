6e353a0d90285586d850c555cd412f8cc073f349
kangho <cannalee90@gmail.com>
2015-10-17 03:45:33 +0900

    bipartite graph

---
+++ BOJ/11378_열혈강호4.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <utility>
+#include <functional>
+#include <limits.h>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <stack>
+
+typedef long long ll;
+using namespace std;
+
+vector<int> work;
+vector<bool> check;
+vector<vector<int > > graph;
+
+bool dfs(int here) {
+	for (int i = 0; i < graph[here].size(); i++) {
+		int there = graph[here][i];
+		if (check[there]) 
+			continue;
+		else {
+			check[there] = true;
+			if (work[there] == -1 || dfs(work[there])) {
+				work[there] = here;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+int main(){
+	freopen("input.txt", "r", stdin);
+	int n, m, k;
+	cin >> n >> m >> k;
+	graph.assign(n + 1, vector<int>());
+	work.assign(m + 1, -1);
+	check.assign(m + 1, false);
+	for (int i = 1; i <= n; i++) {
+		int job;
+		cin >> job;
+		for (int j = 0; j < job; j++) {
+			int num;
+			cin >> num;
+			graph[i].push_back(num);
+		}
+	}
+
+	int ans = 0;
+
+	for (int i = 1; i <= n; i++) {
+		fill(check.begin(), check.end(), false);
+		if (dfs(i))
+			ans++;
+	}
+	while (true) {
+		bool isMatched = false;
+		for (int i = 1; i <= n && k > 0; i++) {
+			fill(check.begin(), check.end(), false);
+			if (dfs(i)) {
+				ans++;
+				k--;
+				isMatched = true;
+			}
+		}
+		if (!isMatched)
+			break;
+	}
+	cout << ans << endl;
+}
