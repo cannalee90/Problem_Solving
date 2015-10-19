e0e910cc25bcb6113c4d5a173e4d5520108cc938
kangho <cannalee90@gmail.com>
2015-10-19 12:34:10 +0900

    bipertite graph

---
+++ BOJ/2188_축사 배정.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <utility>
+#include <functional>
+#include <limits.h>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <stack>
+
+typedef long long ll;
+using namespace std;
+
+vector<int> b;
+vector<bool> check;
+vector<vector<int> > graph;
+
+bool dfs(int here) {
+	for (int &next : graph[here]) {
+		if (check[next])
+			continue;
+		check[next] = true;
+		if (b[next] == -1 || dfs(b[next])) {
+			b[next] = here;
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(){
+	freopen("input.txt", "r", stdin);
+	int n, m;
+	cin >> n >> m;
+	b.assign(m + 1, -1);
+	check.assign(m + 1, false);
+	graph.assign(n + 1, vector<int>());
+	for (int i = 1; i <= n; i++) {
+		int num;
+		cin >> num;
+		for (int j = 0; j < num; j++) {
+			int tmp;
+			cin >> tmp;
+			graph[i].push_back(tmp);
+		}
+	}
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		fill(check.begin(), check.end(), false);
+		if (dfs(i))
+			ans++;
+	}
+	cout << ans << endl;
+}
