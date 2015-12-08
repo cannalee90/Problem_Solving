e7680a41de614948f0f6761f530f6f1c49d1d6c9
cannalee90 <cannalee90@gmail.com>
2015-12-09 02:20:10 +0900

    number theory

---
+++ BOJ/11678_팩토리얼의0의갯수.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+#include <stack>
+#include <map>
+
+#define INF 987654321
+typedef long long ll;
+
+using namespace std;
+
+int fac(int mid) {
+    int ret = 0;
+    int base = 5;
+    while(mid / base) {
+        ret += mid / base;
+        base *= 5;
+    }
+    return ret;
+}
+
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    int n;
+    cin >> n;
+    int left = 1, right = 500000000;
+    int ans = -1;
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        int cntZero = fac(mid);
+        if(cntZero == n) {
+            ans = mid;
+        }
+        if(cntZero < n) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    cout << ans << endl;
+}
+
