a4fc1e271563e08c7df8bf1f13b4a42a8afe06e9
cannalee90 <cannalee90@gmail.com>
2016-02-02 05:33:08 +0900

    simple math and implement

--- BOJ/2399_거리의_차이.cpp
+++ BOJ/2399_거리의_차이.cpp
@@ -2,3 +2,29 @@
 // Created by  이강호 on 2016. 2. 2..
 //
 
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    vector<long long > vl(n);
+    long long sum = 0, partial = 0;
+    for(int i = 0; i < n; i++) {
+
+        scanf("%lld", &vl[i]);
+
+    }
+    sort(vl.rbegin(), vl.rend());
+    for(int i = 0;i < vl.size(); i++)  {
+        long long num = vl[i];
+        sum += (num) * ((n * 2LL) - ((i) * 2LL) - 1);
+        sum -= (num) * ((i * 2LL) + 1LL);
+    }
+    cout << sum << endl;
+}
