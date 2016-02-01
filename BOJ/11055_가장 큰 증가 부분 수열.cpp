36071853591a60f86859e2078db10d0dfc0c549f
cannalee90 <cannalee90@gmail.com>
2016-02-01 23:08:55 +0900

    DP

--- BOJ/11055_가장 큰 증가 부분 수열.cpp
+++ BOJ/11055_가장 큰 증가 부분 수열.cpp
@@ -2,3 +2,39 @@
 // Created by  이강호 on 2016. 2. 1..
 //
 
+#include<stdio.h>
+#include<iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <bitset>
+#include <deque>
+#include <queue>
+#include <tuple>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main() {
+    freopen("/Users/kangho/Documents/Problem_Solving/input.txt", "r", stdin);
+    int n, m;
+    cin >> n;
+    vector<int> vi(n), dp(n);
+    for(int i = 1;i  < n; i++) {
+        cin >> vi[i];
+    }
+    dp[0] = vi[0];
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(vi[j] < vi[i]) {
+                dp[i] = max(dp[i], dp[j] + vi[i]);
+            }
+        }
+    }
+
+    cout << *max_element(dp.begin(), dp.end()) << endl;
+}
+
