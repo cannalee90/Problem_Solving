1804735db90c920e1469e99a1cf10fdf52ccfa9f
cannalee90 <cannalee90@gmail.com>
2016-02-07 23:20:53 +0900

    PQ

--- BOJ/1939_중량제한.cpp
+++ BOJ/1939_중량제한.cpp
@@ -2,3 +2,55 @@
 // Created by  이강호 on 2016. 2. 7..
 //
 
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctype.h>
+#include <cmath>
+#include <algorithm>
+
+typedef long long ll;
+
+#define INF 987654321
+using namespace std;
+
+
+int main() {
+    int n, m;
+    freopen("/Users/kangho/Documents/Problem_Solving/input.txt", "r", stdin);
+    scanf("%d %d", &n, &m);
+    vector<vector<pair<int, int > > > graph(n + 1);
+    for(int i = 0; i < m;i ++) {
+        int u, v, c;
+        scanf("%d %d %d", &u, &v, &c);
+        graph[u].push_back({v, c});
+        graph[v].push_back({u, c});
+    }
+    int source, sink;
+    scanf("%d %d", &source, &sink);
+    priority_queue<pair<int, int > > pq;
+    vector<bool> visited(n + 1, false);
+    pq.push({INF + INF, source});
+    while(!pq.empty()){
+        int here = pq.top().second;
+        int hereMax = pq.top().first;
+        pq.pop();
+        if(here == sink) {
+            cout << hereMax << endl;
+            exit(0);
+        }
+        if(visited[here]) continue;
+        visited[here] = true;
+        for(auto next : graph[here]) {
+            int there = next.first;
+            int thereMax = min(next.second, hereMax);
+            if(!visited[there]) {
+
+                pq.push({thereMax, there});
+            }
+        }
+    }
+}
